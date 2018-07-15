ef9cbccf880c1c745c609cd16aa94b97b79dc989
reichert621 <reichertjalex@gmail.com>
2018-07-15 10:50:57 -0700

    Clean up for deploying to firebase

--- src/App.js
+++ src/App.js
@@ -1,60 +1,127 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// Import CSV data
-import ports from '../data/ports.csv';
-// Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-// Import custom styles
-import './App.less';
+import React from "react";
+import ReactDOM from "react-dom";
+import moment from "moment";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.less";
+
+const Attachment = ({ attachment }) => {
+  if (!attachment || !attachment.type || !attachment.payload) {
+    return null;
+  }
+
+  const { type, payload } = attachment;
+  const uri = payload["firebase-uri"];
+
+  switch (type) {
+    case "image":
+      return <img src={uri} />;
+    case "video":
+      return (
+        <video controls>
+          <source src={uri} />
+        </video>
+      );
+    case "audio":
+      return (
+        <audio controls>
+          <source src={uri} />
+        </audio>
+      );
+  }
+};
+
+const Event = ({ event }) => {
+  if (!event) return null;
+
+  const {
+    message: { text, attachments = [] },
+    timestamp
+  } = event;
+  const prettyDate = moment(+timestamp).format("ddd MMM DD hh:mm a");
+
+  return (
+    <li>
+      <div>
+        <b>{prettyDate}</b>
+      </div>
+      <ul>
+        {text && <li>{text}</li>}
+        {attachments &&
+          attachments.map((att, idx) => {
+            return (
+              <div key={idx} className="attachment-container">
+                <Attachment attachment={att} />
+              </div>
+            );
+          })}
+      </ul>
+    </li>
+  );
+};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { ports: [] };
+    this.state = { data: null };
   }
 
   componentDidMount() {
-    this.setState({ ports });
+    const config = {
+      apiKey: "AIzaSyDCSEZlaELZ3zsQdGsJwdjflcpw8Diqv9M",
+      authDomain: "kareem-2fdc3.firebaseapp.com",
+      databaseURL: "https://kareem-2fdc3.firebaseio.com",
+      projectId: "kareem-2fdc3",
+      storageBucket: "kareem-2fdc3.appspot.com",
+      messagingSenderId: "759332260133"
+    };
+
+    firebase.initializeApp(config);
+
+    const userId = window.location.pathname.slice(1);
+    if (!userId) {
+      this.setState({ data: {} });
+      return;
+    }
+
+    this._ref = firebase.database().ref(`/users/${userId}`);
+    this._ref.on("value", snapshot => {
+      this.setState({ data: snapshot.val() || {} });
+    });
   }
 
   render() {
-    const { ports = [] } = this.state;
+    const { data } = this.state;
+
+    if (!data) {
+      return <div>Loading...</div>;
+    }
+
+    if (Object.keys(data).length === 0) {
+      return <div>Log something!</div>;
+    }
 
     return (
-      <div className="container ports-container">
-        <h1>My Ports!</h1>
-        <div className="ports-table-container">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Code</th>
-                <th>Type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                ports.map(port => {
-                  const { id, name, code, type } = port;
-
-                  return (
-                    <tr key={id}>
-                      <th>{id}</th>
-                      <td>{name}</td>
-                      <td>{code}</td>
-                      <td>{type}</td>
-                    </tr>
-                  );
-                })
-              }
-            </tbody>
-          </table>
+      <div className="container" style={{ paddingTop: 40 }}>
+        <div>
+          <ul>
+            {Object.keys(data)
+              .sort()
+              .reverse()
+              .map(ts => data[ts])
+              .filter(event => event)
+              .map(event => {
+                return <Event key={event.timestamp} event={event} />;
+              })}
+          </ul>
         </div>
+      </div>
+    );
+    return (
+      <div className="container ports-container">
+        <h1>Nutrition!</h1>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/App.js',
+  node: {
+    fs: "empty"
+  },
+  entry: "./src/App.js",
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js"
   },
   resolve: {
-    extensions: ['.js']
+    extensions: [".js"]
   },
-  mode: 'development',
+  mode: "development",
   module: {
     rules: [
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
+        test: /\.jsx?$/,
+        loader: "babel-loader",
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: "style-loader!css-loader"
       },
       {
         test: /\.less$/,
-        loader: 'style-loader!css-loader!less-loader'
-      },
-      {
-        test: /\.csv$/,
-        loader: 'csv-loader',
-        options: {
-          dynamicTyping: true,
-          header: true,
-          skipEmptyLines: true
-        }
+        loader: "style-loader!css-loader!less-loader"
       }
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, 'build'),
-    port: 8000
+    contentBase: path.join(__dirname, "build"),
+    port: 1337,
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: 'body'
+      template: "src/index.html",
+      inject: "body"
     })
   ]
 };
